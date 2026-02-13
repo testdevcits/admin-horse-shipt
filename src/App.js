ad69a8f41a93f6d22e539abd92e8e3e38077a618
testdevcits <testdevcits@gmail.com>
2026-02-13 16:22:04 +0530

    Add Vercel config for deployment

--- src/App.js
+++ src/App.js
@@ -1,9 +1,4 @@
-import React from "react";
-// If you prefer BrowserRouter, keep it
-import { BrowserRouter } from "react-router-dom";
-// Quick fix for static hosting: you can switch to HashRouter
-// import { HashRouter } from "react-router-dom";
-
+import { HashRouter } from "react-router-dom";
 import { AuthProvider } from "./context/AuthContext";
 import { ThemeProvider } from "./context/ThemeContext";
 import { BreedProvider } from "./context/BreedContext";
@@ -11,8 +6,7 @@ import AppRoutes from "./routes/AppRoutes";
 
 function App() {
   return (
-    // BrowserRouter works if vercel.json routes are correct
-    <BrowserRouter>
+    <HashRouter>
       <AuthProvider>
         <ThemeProvider>
           <BreedProvider>
@@ -20,18 +14,7 @@ function App() {
           </BreedProvider>
         </ThemeProvider>
       </AuthProvider>
-    </BrowserRouter>
-
-    // Alternative quick fix for Vercel static hosting:
-    // <HashRouter>
-    //   <AuthProvider>
-    //     <ThemeProvider>
-    //       <BreedProvider>
-    //         <AppRoutes />
-    //       </BreedProvider>
-    //     </ThemeProvider>
-    //   </AuthProvider>
-    // </HashRouter>
+    </HashRouter>
   );
 }
 
